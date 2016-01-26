c8051fe9812e1a3e8e37a1c38b4172dd64288b2b
Crispus <cmmwaura@ucdavis.edu>
2016-01-26 12:43:05 -0800

    first commit

---
+++ products/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+
+from .models import Products
+# Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    '''
+    This will be the admin configuration of each individual product that is entered.
+    With this he will not only be able to search through the products but can edit
+    features of the products from the admin product dashboard.
+    '''
+
+    date_hierarchy = 'timestamp'
+    # adding the items to have a searchable characteristic
+    search_fields = ['title', 'description']
+    # fields in products/models.py
+    list_display = ['title', 'price', 'active', 'updated']
+    # give the admin the edit capabilities in the items
+    list_editable = ['price', 'active']
+    # Sorting out the list based on characteristics
+    list_filter = ['price', 'title']
+
+    # these are the readonly fields
+    readonly_fields = ['updated', 'timestamp']
+
+    # pre-populating the slug field based on the title
+    prepopulated_fields = {"slug": ("title",)}
+
+    class Meta:
+        model = Products
+
+# register the  models and the admins
+admin.site.register(Products, ProductAdmin)
---
+++ products/models.py
@@ -0,0 +1,45 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.core.urlresolvers import reverse
+
+# Create your models here.
+
+
+class Products(models.Model):
+
+    '''
+    these will be the models that will include all the products that will be used by the site.
+    It will borrow from the class of Django Models and in the end it will return the title of
+    product as a __unicode__ method. In addition to this a few things need to be included.
+    1. presence of a Slug field which will be a unique field that will be synchronized with the
+        title. This is what will identify a product for instance at the URL
+    2.
+    '''
+
+    title = models.CharField(max_length=140, blank=False)
+    description = models.TextField(max_length=1000, null=True, blank=True)
+    price = models.DecimalField(max_digits=100, default=29.99, decimal_places=2)
+    sale_price = models.DecimalField(max_digits=100, null=True, blank=True, decimal_places=2)
+    slug = models.SlugField(unique=True)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        
+        '''
+
+        :return: it will return the title of the product  on the admin page.
+        '''
+        return self.title
+
+    class Meta:
+        unique_together = ("title", "slug")
+
+    def get_price(self):
+        return self.price
+
+    def get_absolute_url(self):
+        return reverse("single_product", kwargs={"slug":self.slug})
+        #return "/products/%s/"%(self.slug)
